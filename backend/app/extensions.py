72af897cabb2fb6ccbf9d3c00db4ef8cbc06a2c8
nish <nishchup489@gmail.com>
2025-12-17 17:18:13 -0500

    added extension

---
+++ backend/app/extensions.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_limiter import Limiter
+from flask_login import LoginManager
+from flask_limiter.util import get_remote_address
+from argon2 import PasswordHasher
+from flask_cors import CORS
+
+
+
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+mail = Mail()
+login_manager = LoginManager()
+cors = CORS()
+ph = PasswordHasher(
+    time_cost=2,
+    memory_cost=102400 ,
+    parallelism=8
+)
+limiter = Limiter(
+    key_func=get_remote_address , 
+    default_limits= ["200 per hour"]
+)
