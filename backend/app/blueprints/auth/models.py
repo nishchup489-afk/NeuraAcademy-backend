e78afebf3fd4df5d22829a95200c4e64377b751e
nish <nishchup489@gmail.com>
2025-12-17 20:24:04 -0500

    added auth db model

---
+++ backend/app/blueprints/auth/models.py
@@ -0,0 +1,24 @@
+from app.extensions import db , login_manager
+from flask_login import UserMixin
+from datetime import datetime , timedelta
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+
+class User(UserMixin , db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(UUID(as_uuid=True) , primary_key = True , default= uuid.uuid4)
+    email = db.Column(db.String(200) , nullable=False , unique=True)
+    username = db.Column(db.String(100) , nullable=False)
+    password = db.Column(db.String(200) , nullable=False)
+    confirmed = db.Column(db.Boolean , default=False)
+    role = db.Column(db.String(50) , default="user" )
+    timestamp = db.Column(db.DateTime , default = datetime.now)
+
+class FailedLogin(db.Model):
+    id = db.Column(UUID(as_uuid=True) , primary_key = True , default = uuid.uuid4)
+    email = db.Column(db.String(200) , unique=True , nullable = False)
+    timestamp = db.Column(db.DateTime , default = datetime.dateime.now)
+    ip = db.Column(db.String(50))
+
+
